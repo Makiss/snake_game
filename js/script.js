321e83ee4b1f301f7ef7cd5ae6fd0a061b8be6c8
Maksym <syomushkin.maxim@gmail.com>
2016-07-29 01:59:16 +0300

    Move snake with keys 37-left,38-up,39-right,40-down

--- js/script.js
+++ js/script.js
@@ -4,27 +4,13 @@ $(document).ready(function() {
       width = snakeCanvas.width,
       height = snakeCanvas.height,
       snakeSize = 10,
+      direction = "right",
       snake = [
-        {
-          "x": 0,
-          "y": 0
-        },
-        {
-          "x": 10,
-          "y": 0
-        },
-        {
-          "x": 20,
-          "y": 0
-        },
-        {
-          "x": 30,
-          "y": 0
-        },
-        {
-          "x": 40,
-          "y": 0
-        }
+        { "x": 0, "y": 0 },
+        { "x": 10, "y": 0 },
+        { "x": 20, "y": 0 },
+        { "x": 30, "y": 0 },
+        { "x": 40, "y": 0 }
       ];
 
   var gameLoop = setInterval(reDraw, 50);
@@ -55,6 +41,34 @@ $(document).ready(function() {
 
   function updateSnake(snakeArray) {
     snakeArray.shift();
-    snakeArray.push({"x": snakeArray[snake.length-1].x+10, "y": snakeArray[snake.length-1].y});
+    snakeArray.push(updateDirection(snakeArray, direction));
   }
+
+  function updateDirection(snakeArray, direction) {
+    var cellX = snakeArray[snake.length-1].x,
+        cellY = snakeArray[snake.length-1].y;
+
+    if(direction === "right") {
+      cellX = cellX + 10;
+    } else if(direction === "left") {
+      cellX = cellX - 10;
+    } else if(direction === "up") {
+      cellY = cellY - 10;
+    } else if(direction === "down") {
+      cellY = cellY + 10;
+    }
+    return { "x": cellX, "y": cellY };
+  }
+
+  $(document).on("keydown", function(e) {
+    if(e.which == "37" && direction !== "right") {
+      direction = "left";
+    } else if(e.which == "38" && direction !== "down") {
+      direction = "up";
+    } if(e.which == "39" && direction !== "left") {
+      direction = "right";
+    } if(e.which == "40" && direction !== "up") {
+      direction = "down";
+    }
+  });
 });
