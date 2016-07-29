7f0631fe0a348e6d37f02f47f74a8d2998d4913c
Maksym <syomushkin.maxim@gmail.com>
2016-07-29 03:24:33 +0300

    Add food,score,snake gets bigger eating food,end game when snake meets the wall

--- js/script.js
+++ js/script.js
@@ -5,24 +5,61 @@ $(document).ready(function() {
       height = snakeCanvas.height,
       snakeSize = 10,
       direction = "right",
+      foodX,
+      foodY,
+      gameLoop,
       snake = [
         { "x": 0, "y": 0 },
         { "x": 10, "y": 0 },
         { "x": 20, "y": 0 },
         { "x": 30, "y": 0 },
         { "x": 40, "y": 0 }
-      ];
+      ],
+      gameSpeed = 50,
+      score = 0;
 
-  var gameLoop = setInterval(reDraw, 50);
+  startGame();
+
+  function startGame() {
+    createNewFood();
+    gameLoop = setInterval(reDraw, gameSpeed);
+  }
+
+  function stopGame() {
+    clearInterval(gameLoop);
+  }
 
   function reDraw() {
     drawBg();
     drawSnake(snake);
-  }
+    drawFood();
+    drawScore();
 
+    var collisionStatus = checkCollision(snake, foodX, foodY);
+
+    if(collisionStatus == "food") {
+      score++;
+      createNewFood();
+      snake.unshift(updateDirection(snake, direction));
+    } else if(collisionStatus == "wall") {
+      stopGame();
+      score = 0;
+    } else {
+
+    }
+  }
 
   function drawBg() {
     paint(0, 0, width, height, "white", "black");
+  }
+
+  function drawFood() {
+    paint(foodX * snakeSize, foodY * snakeSize, snakeSize, snakeSize, "green", "black");
+  }
+
+  function drawScore() {
+    context.fillStyle = "grey";
+    context.fillText("Your Score: " + score, 5, height-5);
   }
 
   function drawSnake(snakeArray) {
@@ -71,4 +108,28 @@ $(document).ready(function() {
       direction = "down";
     }
   });
+
+  function createNewFood() {
+    foodX = parseInt(Math.random() * width/snakeSize),
+    foodY = parseInt(Math.random() * height/snakeSize);
+  }
+
+  function checkCollision(snakeArray, foodXInput, foodYInput) {
+    var collision = "nothing";
+    snakeArray.every(function(element) {
+      if(element.x == foodXInput * snakeSize && element.y == foodYInput * snakeSize) {
+        collision = "food";
+        return false;
+      } else if(element.x == -10 ||
+                element.y == -10 ||
+                element.x == width ||
+                element.y == height) {
+        collision = "wall";
+        return false;
+      } else {
+        return true;
+      }
+    });
+    return collision;
+  }
 });
